7af52474f90a096f52b8ad0e58cac1f684b08ba3
tianwei <1258418005@qq.com>
2017-10-23 16:26:25 +0800

    first

---
+++ whawhawhatSwift/Views/TWButton.swift
@@ -0,0 +1,17 @@
+//
+//  TWButton.swift
+//  whawhawhatSwift
+//
+//  Created by tianwei on 2017/10/23.
+//  Copyright © 2017年 whawhawhat. All rights reserved.
+//
+
+import UIKit
+
+class TWButton: UIButton {
+    var index:Int = 0
+    var section:Int = 0
+    var row:Int = 0
+    
+    
+}
---
+++ whawhawhatSwift/whawhawhatSwift.h
@@ -0,0 +1,19 @@
+//
+//  whawhawhatSwift.h
+//  whawhawhatSwift
+//
+//  Created by tianwei on 2017/10/23.
+//  Copyright © 2017年 whawhawhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for whawhawhatSwift.
+FOUNDATION_EXPORT double whawhawhatSwiftVersionNumber;
+
+//! Project version string for whawhawhatSwift.
+FOUNDATION_EXPORT const unsigned char whawhawhatSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <whawhawhatSwift/PublicHeader.h>
+
+
